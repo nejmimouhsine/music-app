5b0850fe3e13bc326b3f749537ae4df802dd1f32
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2021-08-06 14:53:44 +0100

    create the login and register functionalities

---
+++ src/includes/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBUoP7eLDUrUSJ_wr-RblMiOp5PdFIJl78',
+  authDomain: 'music-app-5c31b.firebaseapp.com',
+  projectId: 'music-app-5c31b',
+  storageBucket: 'music-app-5c31b.appspot.com',
+  appId: '1:677653537688:web:81d5c08e5dd91a514a01ed',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users');
+
+export { auth, db, usersCollection };
