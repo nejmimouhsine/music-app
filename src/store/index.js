4f216ef39c340b2b46b596bf12f7405de2e88254
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2021-08-09 18:42:46 +0100

    Adding the functionality of play/stop music

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
+import helpers from '@/includes/helpers';
+import { Howl } from 'howler';
 
 export default createStore({
   state: {
     authModalShow: false,
     userLoggedIn: false,
+    currentSong: {},
+    sound: {},
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%',
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -13,9 +20,28 @@ export default createStore({
     toggleAuth: (state) => {
       state.userLoggedIn = !state.userLoggedIn;
     },
+    newSong: (state, payload) => {
+      state.currentSong = payload;
+      state.sound = new Howl({
+        src: [payload.url],
+        html5: true,
+      });
+    },
+    updatePosition(state) {
+      state.seek = helpers.formatTime(state.sound.seek());
+      state.duration = helpers.formatTime(state.sound.duration());
+      state.playerProgress = `${(state.sound.seek() / state.sound.duration())
+        * 100}%`;
+    },
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
   },
   actions: {
     async register({ commit }, payload) {
@@ -57,6 +83,57 @@ export default createStore({
       if (payload.route.meta.requiresAuth) {
         payload.router.push({ name: 'home' });
       }
+    },
+    async newSong({ commit, state, dispatch }, payload) {
+      commit('newSong', payload);
+
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
+
+      state.sound.play();
+
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
+    },
+    async toggleAudio({ state }) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      if (state.sound.playing()) {
+        state.sound.pause();
+      } else {
+        state.sound.play();
+      }
+    },
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      }
+    },
+    updateSeek({ state, dispatch }, payload) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      const { x, width } = payload.currentTarget.getBoundingClientRect();
+      const clickX = payload.clientX - x;
+      const percentage = clickX / width;
+      const seconds = state.sound.duration() * percentage;
+
+      state.sound.seek(seconds);
+
+      state.sound.once('seek', () => {
+        dispatch('progress');
+      });
     },
   },
 });
